26f6424f7e9b1a77180e3191912ea50b6cc52e26
thakur24 <thakurdhruv200@gmail.com>
2020-11-30 09:37:47 +0530

    My Project

---
+++ code/client (1).c
@@ -0,0 +1,137 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<netinet/in.h>
+#include<netinet/ip.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+int port_number = 20000;
+
+int main(void)
+{
+	int socket_desc;
+	struct sockaddr_in server_addr;
+	char server_message[2000], client_message[2000];
+	memset(server_message,'\0',sizeof(server_message));
+	memset(client_message,'\0',sizeof(client_message));
+	//Creating Socket
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if(socket_desc < 0)
+	{
+		printf("Could Not Create Socket. Error!!!!!\n");
+		return -1;
+	}
+	
+	printf("Socket Created\n");
+	
+	//Specifying the IP and Port of the server to connect
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port_number);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	//------------------------------------------------------------------------------------------------//
+	
+    //Now connecting to the server accept() using connect() from client side
+	if(connect(socket_desc, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+	{
+		printf("Connection Failed. Error!!!!!");
+		return -1;
+	}
+ 
+	//Receive the connection message back from the server		
+	if(recv(socket_desc, server_message, sizeof(server_message),0) < 0)
+	{
+		printf("Receive Failed. Error!!!!!\n");
+		return -1;
+	}
+	
+	printf("Server Message: %s\n\n",server_message);
+	
+	memset(server_message,'\0',sizeof(server_message));
+	memset(client_message,'\0',sizeof(client_message));		
+
+	//Get Input from the User
+	printf("Enter your name and CNIC in format (Ayesha Kerketta/1805409): ");
+	fgets(client_message, 200, stdin);
+
+	//Send credentials back to Server	
+	if(send(socket_desc, client_message, strlen(client_message),0) < 0)
+	{
+		printf("Send Failed. Error!!!!\n");
+		return -1;
+	}		
+
+				
+        memset(server_message,'\0',sizeof(server_message));
+	memset(client_message,'\0',sizeof(client_message));
+	
+	//Receive the message back from the server		
+	if(recv(socket_desc, server_message, sizeof(server_message),0) < 0)
+	{
+		printf("Receive Failed. Error!!!!!\n");
+		return -1;
+	}
+
+	printf("Server Message: %s\n\n",server_message);
+
+	if (strcmp(server_message, "Sorry! Your name is not in the authorized voters list.\n") != 0)
+	{
+		//Receive the message back from the server		
+		if(recv(socket_desc, server_message, sizeof(server_message),0) < 0)
+		{
+			printf("Receive Failed. Error!!!!!\n");
+			return -1;
+		}
+
+		printf("Server Message: %s\n\n",server_message);
+
+		if (strcmp(server_message, "Sorry! You have already casted a vote. You can't vote again.\n") != 0)
+		{
+			memset(server_message,'\0',sizeof(server_message));
+	                memset(client_message,'\0',sizeof(client_message));
+			//Get Input Symbol from the User	
+			printf("Enter the candidate's symbol: ");
+			fgets(client_message, 200, stdin);
+		
+			//Send the symbol back to server			
+			if(send(socket_desc, client_message, strlen(client_message),0) < 0)
+			{
+				printf("Send Failed. Error!!!!\n");
+				return -1;
+			}		
+			
+			//Receive confirmation from server		
+			if(recv(socket_desc, server_message, sizeof(server_message),0) < 0)
+			{
+				printf("Receive Failed. Error!!!!!\n");
+				return -1;
+			}
+
+			printf("Server Message: %s\n\n",server_message);
+		}
+					
+	}
+	else
+		{memset(server_message,'\0',sizeof(server_message));
+		 memset(client_message,'\0',sizeof(client_message));		
+
+		//Get Input from the User
+		printf("Enter your name and CNIC in format (Ayesha Kerketta/1805409): ");
+		fgets(client_message, 200, stdin);
+		//Send credentials back to Server	
+		if(send(socket_desc, client_message, strlen(client_message),0) < 0)
+			{
+			printf("Send Failed. Error!!!!\n");
+			return -1;
+			}
+					
+		}
+
+	//Closing the Socket
+	close(socket_desc);
+	return 0;
+}
+
